688bba3a4d2314c55a36e41c80463f00ddef14d7
Tommy Robertson <trobe745@gmail.com>
2020-04-08 22:48:04 -0500

    Removed .sqlite3 to add to .gitignore

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
-browser = webdriver.Firefox(executable_path='E:\\Dropbox\\Documents\\self_learning\\tdd_project_folder\\geckodriver.exe')
+browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
 
 assert 'Django' in browser.title
