6ec4a3a3aab837c968e52669e8b1466af55388ef
Tommy Robertson <trobe745@gmail.com>
2020-04-27 22:36:43 -0500

    Rename all item input ids and names. Currently still not in a working state

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -8,7 +8,7 @@ class ItemValidationTest(FunctionalTest):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
@@ -18,13 +18,13 @@ class ItemValidationTest(FunctionalTest):
         ))
 
         # She tries again with some text for the item, which now works
-        self.browser.find_element_by_id('id_new_item').send_keys("Retry")
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys("Retry")
+        self.get_item_input_box().send_keys(Keys.ENTER)
         #self.assertNotIn("You can't have an empty list item", self.browser.page_source)
         self.wait_for_row_in_list_table("1: Retry")
 
         # Perversely, she now decides to submit a second blank list item
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
         # She receives a similar warning on the list page
         self.wait_for(lambda: self.assertEqual(
@@ -33,8 +33,8 @@ class ItemValidationTest(FunctionalTest):
         ))
 
         # And she can corrext it by filling some text in
-        self.browser.find_element_by_id('id_new_item').send_keys("Retry twice")
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys("Retry twice")
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: Retry")
         self.wait_for_row_in_list_table("2: Retry twice")
         #self.assertNotIn("You can't have an empty list item", self.browser.page_source)
