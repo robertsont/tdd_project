6b88253e5034912739ce7f6c5cbc8e5283416d24
Tommy Robertson <trobe745@gmail.com>
2020-07-05 15:13:22 -0500

    Fixed server_tools to correctly use identity file.

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -1,7 +1,9 @@
 from fabric.api import run
 from fabric.context_managers import settings, shell_env
+import os
 
-SERVER_IP = '157.245.209.108'
+SERVER_IP = os.environ["SERVER_IP"]
+IDENTITY_FILE = os.environ["IDENTITY_FILE"]
 
 def _get_manage_dot_py(host):
     return f'~/sites/{host}/virtualenv/bin/python ~/SITES/{host}/manage.py'
@@ -10,11 +12,11 @@ def _get_manage_dot_py(host):
 def reset_database(host):
     manage_dot_py = _get_manage_dot_py(host)
     with settings(host_string=f'trobe@{SERVER_IP}'):
-        run(f'{manage_dot_py} flush --noinput')
+        run(f'{manage_dot_py} flush --noinput -i {IDENTITY_FILE}')
 
 
 def _get_server_env_vars(host):
-    env_lines = run(f'cat ~/sites/{host}.env').splitlines()
+    env_lines = run(f'cat ~/sites/{host}.env -i {IDENTITY_FILE}').splitlines()
     return dict(l.split('=') for l in env_lines if l)
 
 
@@ -23,5 +25,5 @@ def create_session_on_server(host, email):
     with settings(host_string=f'trobe@{SERVER_IP}'):
         env_vars = _get_server_env_vars(host)
         with shell_env(**env_vars):
-            session_key = run(f'{manage_dot_py} create_session {email}')
+            session_key = run(f'{manage_dot_py} create_session {email} -i {IDENTITY_FILE}')
             return session_key.strip()
