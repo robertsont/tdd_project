9bddb50cdaf96ee662b3deef7d3f43085efeaea1
Tommy Robertson <trobe745@gmail.com>
2020-07-19 15:55:09 -0500

    URL, placeholder view, and first-cut templates for my_lists

--- lists/views.py
+++ lists/views.py
@@ -23,3 +23,6 @@ def new_list(request):
         form.save(for_list=list_)
         return redirect(list_)
     return render(request, 'home.html', {'form': form})
+
+def my_lists(request, email):
+    return render(request, 'my_lists.html')
