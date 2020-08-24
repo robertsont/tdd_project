011c9c0f686754f1894b345ab651563e50fc5f3b
Tommy Robertson <trobe745@gmail.com>
2020-08-23 21:01:59 -0500

    add screenshot on failure to FT runner

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -2,11 +2,16 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
-import time, os
+import time
+import os
+from datetime import datetime
 from .server_tools import reset_database
 
 MAX_WAIT = 10
 
+SCREEN_DUMP_LOCATION = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'screendumps'
+)
 
 class FunctionalTest(StaticLiveServerTestCase):
     def setUp(self):
@@ -22,7 +27,40 @@ class FunctionalTest(StaticLiveServerTestCase):
             reset_database(os.environ.get("STAGING_SERVER"))
 
     def tearDown(self):
+        if self._test_has_failed():
+            if not os.path.exists(SCREEN_DUMP_LOCATION):
+                os.makedirs(SCREEN_DUMP_LOCATION)
+            for ix, handle in enumerate(self.browser.window_handles):
+                self._windowid = ix
+                self.browser.switch_to_window(handle)
+                self.take_screenshot()
+                self.dump_html()
         self.browser.quit()
+        super().tearDown()
+    
+    def _test_has_failed(self):
+        return any(error for (method, error) in self._outcome.errors)
+    
+    def take_screenshot(self):
+        filename = self._get_filename() + '.png'
+        print('screenshotting to', filename)
+        self.browser.get_screenshot_as_file(filename)
+    
+    def dump_html(self):
+        filename = self._get_filename() + '.html'
+        print('dumping page HTML to', filename)
+        with open(filename, 'w') as f:
+            f.write(self.browser.page_source)
+    
+    def _get_filename(self):
+        timestamp = datetime.now().isoformat().replace(':', '.')[:19]
+        return '{folder}/{classname}.{method}-window{windowid}-{timestampe}'.format(
+            folder=SCREEN_DUMP_LOCATION,
+            classname=self.__class__.__name__,
+            method=self._testMethodName,
+            windowis=self._windowid,
+            timestamp=timestamp
+        )
 
     def wait(fn):
         def modified_fn(*args, **kargs):
